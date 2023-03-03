954e374380260118c35092b3bc665083d3c18146
Vidar Eldøy <vidar@eldoy.com>
2023-03-03 11:47:41 +0100

    Upgrade to cuid2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const util = require('util')
 const crypto = require('crypto')
 const _ = require('lodash')
 const { v4: uuidv4 } = require('uuid')
-const cuid = require('cuid')
+const cuid = require('@paralleldrive/cuid2').createId
 const bcrypt = require('bcryptjs')
 const yaml = require('js-yaml')
 const sh = require('shelljs')
@@ -18,8 +18,10 @@ extras.regexp = {}
 extras.regexp.email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 extras.regexp.id = /^[a-z0-9]{24,}$/
 extras.regexp.reg = /^%r\/(.+)\/([ig]{0,2})/
-extras.regexp.date = /^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))$/
-extras.regexp.url = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
+extras.regexp.date =
+  /^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))$/
+extras.regexp.url =
+  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
 extras.regexp.ip = /^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$/
 extras.regexp.image = /\.(gif|jpe?g|tiff|png|bmp|svg)$/i
 extras.regexp.subdomain = /^[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]$/
@@ -27,59 +29,61 @@ extras.uuid = uuidv4
 extras.cuid = cuid
 extras.lodash = _
 
-extras.hash = function(str, saltRounds = 10) {
+extras.hash = function (str, saltRounds = 10) {
   return bcrypt.hashSync(String(str), saltRounds)
 }
 
-extras.md5 = function(str) {
+extras.md5 = function (str) {
   return crypto.createHash('md5').update(str).digest('hex')
 }
 
-extras.hex = function(size = 20) {
+extras.hex = function (size = 20) {
   return crypto.randomBytes(size).toString('hex')
 }
 
-extras.compare = function(plain, hash) {
+extras.compare = function (plain, hash) {
   return bcrypt.compareSync(String(plain), String(hash))
 }
 
-extras.isEmail = function(str) {
+extras.isEmail = function (str) {
   return extras.regexp.email.test(String(str))
 }
 
-extras.isId = function(str) {
+extras.isId = function (str) {
   return extras.regexp.id.test(String(str))
 }
 
-extras.isRegExp = function(str) {
+extras.isRegExp = function (str) {
   return extras.regexp.reg.test(String(str))
 }
 
-extras.isDate = function(str) {
+extras.isDate = function (str) {
   return extras.regexp.date.test(String(str))
 }
 
-extras.isURL = function(str) {
+extras.isURL = function (str) {
   return extras.regexp.url.test(String(str))
 }
 
-extras.parseBool = function(v) {
+extras.parseBool = function (v) {
   return ['false', 'null', 'NaN', 'undefined', '0'].includes(v) ? false : !!v
 }
 
 // Find object type
-extras.type = function(obj) {
+extras.type = function (obj) {
   return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
 }
 
 // Format and interpolate strings
-extras.format = function(str, ...args) {
+extras.format = function (str, ...args) {
   for (let i = 0; i < args.length; i++) {
     let val = args[i]
     if (Array.isArray(val)) {
       val = val.join(', ')
     } else if (_.isPlainObject(val)) {
-      val = Object.entries(val).map(x => x.join(': ')).join(', ')
+      val = Object.entries(val)
+        .map((x) => x.join(': '))
+        .join(', ')
     }
     str = str.replace('%s', val)
   }
@@ -87,14 +91,17 @@ extras.format = function(str, ...args) {
 }
 
 // Inspect object
-extras.inspect = function(obj, options = {}) {
+extras.inspect = function (obj, options = {}) {
   const _obj = _.cloneDeep(obj)
   if (options.exclude) {
     for (const opt of options.exclude) {
       _.set(_obj, opt, null)
     }
   }
-  const result = util.inspect(_obj, { showHidden = true, depth = null, colors = true } = options)
+  const result = util.inspect(
+    _obj,
+    ({ showHidden = true, depth = null, colors = true } = options)
+  )
   if (!options.quiet) {
     console.log(result)
   }
@@ -102,7 +109,7 @@ extras.inspect = function(obj, options = {}) {
 }
 
 // Trim strings in object
-extras.trim = function(obj) {
+extras.trim = function (obj) {
   for (const key in obj) {
     if (typeof obj[key] === 'object') {
       extras.trim(obj[key])
@@ -113,12 +120,15 @@ extras.trim = function(obj) {
 }
 
 // Strip strings, multi-line support
-extras.strip = function(str, sep = '\n') {
-  return str.split(sep).map(s => s.trim()).join(sep)
+extras.strip = function (str, sep = '\n') {
+  return str
+    .split(sep)
+    .map((s) => s.trim())
+    .join(sep)
 }
 
 // Transform object string values to Javascript native objects
-extras.transform = function(obj, opt = {}) {
+extras.transform = function (obj, opt = {}) {
   const simple = typeof obj != 'object'
   if (simple) obj = [obj]
 
@@ -144,25 +154,32 @@ extras.transform = function(obj, opt = {}) {
 }
 
 // Convert values based on type
-extras.convert = function(v, type) {
+extras.convert = function (v, type) {
   switch (type) {
-    case 'string': return String(v)
-    case 'id': return String(v)
-    case 'integer': return parseInt(v)
-    case 'float': return parseFloat(v)
-    case 'date': return new Date(v)
-    case 'boolean': return extras.parseBool(v)
-    default: return v
+    case 'string':
+      return String(v)
+    case 'id':
+      return String(v)
+    case 'integer':
+      return parseInt(v)
+    case 'float':
+      return parseFloat(v)
+    case 'date':
+      return new Date(v)
+    case 'boolean':
+      return extras.parseBool(v)
+    default:
+      return v
   }
 }
 
 // Convert object to dot notation
-extras.dot = function(obj, sep = '.') {
+extras.dot = function (obj, sep = '.') {
   const dotted = {}
   function build(obj, str) {
-    for(var key in obj) {
+    for (var key in obj) {
       const trail = str ? `${str}${sep}${key}` : key
-      if(_.isPlainObject(obj[key])) {
+      if (_.isPlainObject(obj[key])) {
         build(obj[key], trail)
       } else {
         dotted[trail] = obj[key]
@@ -174,7 +191,7 @@ extras.dot = function(obj, sep = '.') {
 }
 
 // Unpack object with dot notation
-extras.undot = function(obj, sep = '.') {
+extras.undot = function (obj, sep = '.') {
   const un = {}
   function build(obj, str) {
     for (const key in obj) {
@@ -191,9 +208,9 @@ extras.undot = function(obj, sep = '.') {
 }
 
 // Remove null and undefined from object, or optional types
-extras.clean = function(data, ...types) {
+extras.clean = function (data, ...types) {
   function match(val) {
-    return !types.length && val == null || types.includes(extras.type(val))
+    return (!types.length && val == null) || types.includes(extras.type(val))
   }
 
   function build(obj) {
@@ -214,7 +231,7 @@ extras.clean = function(data, ...types) {
 }
 
 // Get base and extension
-extras.basext = function(file) {
+extras.basext = function (file) {
   const name = path.basename(file)
   let base = name
   let ext = ''
@@ -226,43 +243,50 @@ extras.basext = function(file) {
 }
 
 // Turn a string into a valid slug for web addresses
-extras.slug = function(string) {
-  const a = 'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;'
-  const b = 'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------'
+extras.slug = function (string) {
+  const a =
+    'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;'
+  const b =
+    'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------'
   const p = new RegExp(a.split('').join('|'), 'g')
 
-  return string.toString().toLowerCase()
-  .replace(/\s+/g, '-')
-  .replace(p, c => b.charAt(a.indexOf(c)))
-  .replace(/&/g, '-')
-  .replace(/[^\w\-]+/g, '')
-  .replace(/\-\-+/g, '-')
-  .replace(/^-+/, '')
-  .replace(/-+$/, '')
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(p, (c) => b.charAt(a.indexOf(c)))
+    .replace(/&/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
 }
 
 // Check if file exists
-extras.exist = function(file) {
+extras.exist = function (file) {
   file = extras.resolve(file)
   return fs.existsSync(file)
 }
 
 // Read file
-extras.read = function(file, encoding) {
+extras.read = function (file, encoding) {
   file = extras.resolve(file)
   const [base, ext] = extras.basext(file)
-  if (NODE_EXTENSIONS.includes(encoding) || !encoding && NODE_EXTENSIONS.includes(ext)) {
+  if (
+    NODE_EXTENSIONS.includes(encoding) ||
+    (!encoding && NODE_EXTENSIONS.includes(ext))
+  ) {
     return require(file)
   }
   const content = fs.readFileSync(file, encoding || 'utf8')
-  if (encoding == 'yml' || !encoding && ext == 'yml') {
+  if (encoding == 'yml' || (!encoding && ext == 'yml')) {
     return yaml.load(content) || {}
   }
   return content
 }
 
 // Write file
-extras.write = function(file, content) {
+extras.write = function (file, content) {
   file = extras.resolve(file)
   if (Array.isArray(content) || _.isPlainObject(content)) {
     if (file.endsWith('.json')) {
@@ -277,13 +301,13 @@ extras.write = function(file, content) {
 }
 
 // Append to file
-extras.append = function(file, content) {
+extras.append = function (file, content) {
   file = extras.resolve(file)
   return fs.appendFileSync(file, content)
 }
 
 // Edit file
-extras.edit = function(file, fn) {
+extras.edit = function (file, fn) {
   file = extras.resolve(file)
   const content = extras.read(file, 'utf8')
   const result = fn(content) || ''
@@ -291,51 +315,51 @@ extras.edit = function(file, fn) {
 }
 
 // Exit program
-extras.exit = function(msg, code = 1) {
+extras.exit = function (msg, code = 1) {
   console.log(msg)
   process.exit(code)
 }
 
 // Get command input
-extras.get = function(cmd) {
+extras.get = function (cmd) {
   return extras.run(cmd, { silent: true }).stdout.trim()
 }
 
 // Get terminal input
-extras.input = async function(str = '> ', opt = {}) {
+extras.input = async function (str = '> ', opt = {}) {
   opt = {
     input: process.stdin,
     output: process.stdout,
     ...opt
   }
   const rl = readline.createInterface(opt)
-  const answer = await new Promise(r => rl.question(str, r))
+  const answer = await new Promise((r) => rl.question(str, r))
   rl.close()
   return answer
 }
 
 // Get keypress from stdin
-extras.key = function(fn) {
+extras.key = function (fn) {
   readline.emitKeypressEvents(process.stdin)
   process.stdin.setRawMode(true)
   process.stdin.on('keypress', fn)
 }
 
 // Read directory
-extras.dir = function(file) {
+extras.dir = function (file) {
   file = extras.resolve(file)
   return fs.readdirSync(file)
 }
 
 // Copy files
-extras.copy = function(from, to) {
+extras.copy = function (from, to) {
   from = extras.resolve(from)
   to = extras.resolve(to)
   return sh.cp('-R', from, to)
 }
 
 // Is directory?
-extras.isDir = function(file) {
+extras.isDir = function (file) {
   file = extras.resolve(file)
   try {
     return fs.lstatSync(file).isDirectory()
@@ -345,7 +369,7 @@ extras.isDir = function(file) {
 }
 
 // Is file?
-extras.isFile = function(file) {
+extras.isFile = function (file) {
   file = extras.resolve(file)
   try {
     return fs.lstatSync(file).isFile()
@@ -355,22 +379,22 @@ extras.isFile = function(file) {
 }
 
 // Run command
-extras.run = function(command, options = {}) {
+extras.run = function (command, options = {}) {
   return sh.exec(command, options)
 }
 
 // Make directory
-extras.mkdir = function(...dirs) {
+extras.mkdir = function (...dirs) {
   return sh.mkdir('-p', ...dirs)
 }
 
 // Remove directory
-extras.rmdir = function(...dirs) {
+extras.rmdir = function (...dirs) {
   return sh.rm('-rf', ...dirs)
 }
 
 // Rename file
-extras.rename = function(from, to) {
+extras.rename = function (from, to) {
   from = extras.resolve(from)
   to = extras.resolve(to)
   if (extras.exist(from)) {
@@ -379,7 +403,7 @@ extras.rename = function(from, to) {
 }
 
 // Resolve path
-extras.resolve = function(...dirs) {
+extras.resolve = function (...dirs) {
   let file = dirs.join(path.sep)
   if (file.startsWith(`.${path.sep}`)) {
     file = file.replace('.', process.cwd())
@@ -392,10 +416,10 @@ extras.resolve = function(...dirs) {
 }
 
 // Walk directory tree
-extras.walk = function(dir, fn) {
-  extras.dir(dir).forEach(f => {
+extras.walk = function (dir, fn) {
+  extras.dir(dir).forEach((f) => {
     const file = path.join(dir, f)
-    if(extras.isDir(file)){
+    if (extras.isDir(file)) {
       extras.walk(file, fn)
     } else {
       fn(file, dir)
@@ -404,18 +428,18 @@ extras.walk = function(dir, fn) {
 }
 
 // Directory tree as flat array
-extras.tree = function(root) {
+extras.tree = function (root) {
   root = extras.resolve(root)
   if (!extras.exist(root)) return []
   const files = []
-  extras.walk(root, function(file) {
+  extras.walk(root, function (file) {
     files.push(file)
   })
   return files
 }
 
 // Sort by number
-extras.sortByNumber = function(arr, fn) {
+extras.sortByNumber = function (arr, fn) {
   if (!fn) {
     fn = (a, b) => (a.match(/^\d+/g) || a) - (b.match(/^\d+/g) || b)
   }
@@ -423,8 +447,8 @@ extras.sortByNumber = function(arr, fn) {
 }
 
 // Sleep
-extras.sleep = function(s = 1) {
-  return new Promise(r => setTimeout(r, s * 1000))
+extras.sleep = function (s = 1) {
+  return new Promise((r) => setTimeout(r, s * 1000))
 }
 
 module.exports = extras
